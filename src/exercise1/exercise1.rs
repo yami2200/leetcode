e5f9faa1284cd65f91a5792aa234f1ced6d9aed4
yami2200 <TheYami22@gmail.com>
2021-10-15 15:40:05 +0200

    Exercise 1 Optimised

--- src/exercise1/exercise1.rs
+++ src/exercise1/exercise1.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+// ################### BRUTE FORCE
 pub fn two_sum(nums: &Vec<i32>, target: i32) -> Vec<i32> {
     for i in 0..nums.len(){
         for j in i+1..nums.len(){
@@ -6,5 +9,15 @@ pub fn two_sum(nums: &Vec<i32>, target: i32) -> Vec<i32> {
             }
         }
     }
-    return Vec::new();
+    Vec::new()
+}
+
+// ################## MORE OPTIMISED
+pub fn two_sum2(nums: &Vec<i32>, target: i32) -> Vec<i32> {
+    let mut map: HashMap<i32, usize> = HashMap::new();
+    for i in 0..nums.len(){
+        if let Some(j) = map.get(&(target - nums[i])) { return vec![*j  as i32, i as i32]; }
+        map.insert(nums[i], i);
+    }
+    Vec::new()
 }
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn test_exercise1(){
     let nums = vec![2, 7, 11, 15];
     let target = 9;
     assert_eq!(exercise1::exercise1::two_sum(&nums, target), vec![0, 1]);
+    assert_eq!(exercise1::exercise1::two_sum2(&nums, target), vec![0, 1]);
 }
 
 fn test_exercise9(){
