fceda21d42837d2f00389c36e598b83318b56fdd
yami2200 <TheYami22@gmail.com>
2021-10-15 13:48:28 +0200

    Exercise 1 Done

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 mod exercise1;
 
 fn main() {
-    println!("Hello, Mael!");
-    exercise1::exercise1::solve();
+    test_exercise1();
+}
+
+fn test_exercise1(){
+    let nums = vec![2, 7, 11, 15];
+    let target = 9;
+    println!("{:?}", exercise1::exercise1::two_sum(&nums, target));
 }
